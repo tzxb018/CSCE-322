53f8bd6c4a1a879b090138d7f0d64473fa4f1885
Tomo Bessho <tomobessho018@gmail.com>
2020-09-28 11:31:52 -0500

    finsihed part 1 of homework

--- Assignment 2/helpers.js
+++ Assignment 2/helpers.js
@@ -1,35 +1,33 @@
 module.exports = {
-    readPuzzleFile: readPuzzleFile,
-    readMovesFile: readMovesFile
+  readPuzzleFile: readPuzzleFile,
+  readMovesFile: readMovesFile,
 };
 
-function readPuzzleFile(file)
-{
-    var text;
-    var lines = new Array();
-    var rows = new Array();
-    var filesystem = require('fs');
+function readPuzzleFile(file) {
+  var text;
+  var lines = new Array();
+  var rows = new Array();
+  var filesystem = require("fs");
 
-    text = filesystem.readFileSync(file);
-    lines = text.toString().split("\n");
+  text = filesystem.readFileSync(file);
+  lines = text.toString().split("\n");
 
-    for (var r = 0; r < lines.length; r++) {
-        rows[r] = lines[r].split(",");
-    }
-    
-    rows.splice( rows.length - 1 , 1 );
+  for (var r = 0; r < lines.length; r++) {
+    rows[r] = lines[r].split(",");
+  }
 
-    return rows;
+  rows.splice(rows.length - 1, 1);
+
+  return rows;
 }
 
-function readMovesFile(file)
-{
-    var moves = new Array();
-    var filesystem = require('fs');
-    var line = filesystem.readFileSync(file);
-    var temp = new Array();
-    temp = line.toString().split("\n");
-    moves = temp[0].split(",");
-    
-    return moves
+function readMovesFile(file) {
+  var moves = new Array();
+  var filesystem = require("fs");
+  var line = filesystem.readFileSync(file);
+  var temp = new Array();
+  temp = line.toString().split("\n");
+  moves = temp[0].split(",");
+
+  return moves;
 }
