caf2cf82ee1f7935ec266ffd26101dd6533b20f3
Tomo Bessho <tomobessho018@gmail.com>
2020-09-28 16:59:27 -0500

    moved around test files and finished hw2

--- Assignment 2/template02.js
+++ Assignment 2/template02.js
@@ -1,13 +1,15 @@
-var helpers = require( './helpers' );
-var part = require( './csce322hmwrk02prt02' );
+var helpers = require("./helpers");
+var part = require("./csce322hmwrk02prt02");
 
-var puzzle = helpers.readPuzzleFile( 'part02test01.puzzle.bpf' );
-var moves = helpers.readMovesFile( 'part02test01.moves.bpf' );
+var puzzle = helpers.readPuzzleFile(
+    "./Part 2 Solutions/part02test03.puzzle.bpf"
+);
+var moves = helpers.readMovesFile("./Part 2 Solutions/part02test03.moves.bpf");
 
-var theFunction = part.manyMoves( puzzle );
+var theFunction = part.manyMoves(puzzle);
 
-var after = theFunction( moves );
+var after = theFunction(moves);
 
-for( var i = 0; i < after.length; i++ ){
-    console.log( after[i] );
+for (var i = 0; i < after.length; i++) {
+    console.log(after[i]);
 }
