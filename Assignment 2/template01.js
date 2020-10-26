88397842366d21ea4702a85ac57b8fbf344c8fbf
Tomo Bessho <tbessho2@unl.edu>
2020-10-26 10:45:56 -0500

    removed unnessary java files

--- Assignment 2/template01.js
+++ Assignment 2/template01.js
@@ -1,8 +1,8 @@
 var helpers = require("./helpers");
 var part = require("./csce322hmwrk02prt01");
 
-var puzzle = helpers.readPuzzleFile("part01test18.puzzle.bpf");
-var moves = helpers.readMovesFile("part01test18.moves.bpf");
+var puzzle = helpers.readPuzzleFile("./Part 1 Solutions/part01test01.puzzle.bpf");
+var moves = helpers.readMovesFile("./Part 1 Solutions/part01test01.moves.bpf");
 
 var theFunction = part.oneMove(puzzle);
 
