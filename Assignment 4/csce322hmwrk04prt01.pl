ce2c2c33c83730b31b9279e2a19ba0e5ae84830f
Tomo Bessho <tbessho2@unl.edu>
2020-11-11 14:20:36 -0600

    completed part 1!:

--- Assignment 4/csce322hmwrk04prt01.pl
+++ Assignment 4/csce322hmwrk04prt01.pl
@@ -1,3 +1,43 @@
 oddWidthEvenInversions(Puzzle):-
-    length(Puzzle,PuzzleLength),
-    PuzzleLength > 2.
+    findWidth(Puzzle, RowSize),
+    1 is mod(RowSize, 2),
+    flattenMatrix(Puzzle, FlatPuzzle),
+    iterateList(FlatPuzzle, Inversions, Sum),
+    0 is mod(Sum, 2).
+
+findWidth([H|T], Row):-
+    length(H, Row).
+
+flattenMatrix([],[]).
+
+flattenMatrix([H|T], FlatPuzzle):-
+    flattenMatrix(T, UpdatedFlatPuzzle),
+    append(H, UpdatedFlatPuzzle, FlatPuzzle).
+
+reverse([],Z,Z).
+
+reverse([H|T],Z,Acc) :- reverse(T,Z,[H|Acc]).
+
+iterateList([],0, 0).
+
+iterateList([H|T], Inversions2, Sum2):-
+    countInversions(H, T, Inversions3), 
+    iterateList(T, Inversions4, Sum4),
+    Sum2 is Sum4 + Inversions3.
+
+
+countInversions(H, [], 0).
+
+countInversions(H, [TH|TT], Inversions):-
+    H > TH,
+    TH > 0,
+    countInversions(H, TT, Inversions1), Inversions is Inversions1 + 1.
+
+countInversions(H, [TH|TT], Inversions):-
+    H > TH,
+    TH = 0,
+    countInversions(H, TT, Inversions).
+
+countInversions(H, [TH|TT], Inversions):-
+    H =< TH,
+    countInversions(H, TT, Inversions).
--- Assignment 4/csce322hmwrk04prt02.pl
+++ Assignment 4/csce322hmwrk04prt02.pl
@@ -1,1 +1,4 @@
 quickestWin(Puzzle,[a,b,l,l,l,c]).
+
+nth2D(List, Row, Column, Val):- nth0(Row,List,ListRow), nth0(Column, ListRow, Val).
+findall(I, nth0(I,List,7)),Res).
